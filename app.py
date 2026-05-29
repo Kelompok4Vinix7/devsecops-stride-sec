dcfb53af10568620271492c2399c112ca3fb0692
Kelompok4Vinix7 <kelompok4vinix7@gmail.com>
2026-05-29 14:50:24 +0000

    feat: tambah fitur login

---
+++ app.py
@@ -0,0 +1,11 @@
+# SIMULASI SECRET LEAKAGE
+# Ini adalah contoh kode yang menyimpan kredensial secara hardcoded
+
+DB_PASSWORD = "super-secret-password-123"
+API_KEY = "AIzaSyA-ContohApiKeyGCP-12345"
+
+def login():
+    print(f"Menghubungkan ke database dengan password: {DB_PASSWORD}")
+
+if __name__ == "__main__":
+    login()
